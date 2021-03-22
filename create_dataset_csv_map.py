1b263c6db6718dbbe517dd2573e76c59257bc3cf
Paht J <paht.juangphanich@gmail.com>
2021-03-22 00:47:43 -0400

    Update create_dataset_csv_map.py
    This file now works and uses 16 cores :) Much faster than single thread.

--- create_dataset_csv_map.py
+++ create_dataset_csv_map.py
@@ -5,10 +5,21 @@
 from data_utils import recursive_search
 import imageio 
 import pandas as pd
+from tqdm import trange
+from multiprocessing import Process, Manager
+import os.path as osp
 
-def get_image_size(filename):
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def get_image_size(filename:str):
     h = 0
     w = 0
+    if (not osp.exists(filename)):
+        return {'filename':filename,'height':0,'width':0,'depth':0}
+
     img = imageio.imread(filename)
     if len(img.shape)>2:
         h, w, d = img.shape
@@ -18,9 +29,32 @@ def get_image_size(filename):
         d=1
         return {'filename':filename,'height':h,'width':w,'depth':d}
 
+def evaluate_chunk(L,images):
+    image_data = list()
+    for img in images:
+        image_data.append(get_image_size(img))
+    L.extend(image_data)                                    # append data to master list L
+
+num_cores = 16 
+
 if __name__ == '__main__':
     folder = r'D:/datasets/imagenet-object-localization-challenge/imagenet_object_localization_patched2019/ILSVRC\Data/CLS-LOC/'
     images,_ = recursive_search(folder)
-    image_data = [get_image_size(x) for x in images]
-    df = pd.DataFrame(image_data)    
+
+    image_chunks = list(chunks(images,5000))                # Breaks the list into chunks of a particular size
+    image_info = list()
+    for i in trange(0,len(image_chunks),num_cores):
+        with Manager() as manager:
+            L = manager.list()  # <-- can be shared between processes.
+            processes = list()
+            for core in range(num_cores):
+                p = Process(target=evaluate_chunk, args=(L,image_chunks[i+core]))  # Each process runs on a chunk of data
+                p.start()
+                processes.append(p)
+
+            for p in processes:
+                p.join()                                    # Joins all the data 
+            image_info.extend(L)                            # Adds the data to image_info which will be converted to a dataframe
+            processes.clear()
+    df = pd.DataFrame(image_info)
     df.to_csv('data/dataset_files.csv')
